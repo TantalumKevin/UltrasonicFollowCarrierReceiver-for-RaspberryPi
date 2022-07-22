ef49ae1a7d943970a44a6b01ab2076fdff529441
school_pi <tantalum_zhao@qq.com>
2022-07-22 11:52:20 -0500

    底层驱动深度测试

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import motor,time
 import numpy as np
 
 #       en,in1,in2
-ctrl = [[3,5,7],[8,10,12],[11,13,15],[19,21,23]]
+ctrl = [[3,5,7],[8,10,12]]
 
-mt = motor.Motor(ctrl[0])
+mt0 = motor.Motor(ctrl[0])
+mt1 = motor.Motor(ctrl[1])
 for i in range(-100,100):
-    mt.run(i/100)
+    mt1.run(i/100)
+    mt2.run(i/100)
     time.sleep(0.02)
