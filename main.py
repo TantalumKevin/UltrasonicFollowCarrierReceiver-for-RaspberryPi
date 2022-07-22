b6ead49e509af62e8b181b7121d9e57c1a7b832c
school_pi <tantalum_zhao@qq.com>
2022-07-22 11:52:44 -0500

    小问题

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ ctrl = [[3,5,7],[8,10,12]]
 mt0 = motor.Motor(ctrl[0])
 mt1 = motor.Motor(ctrl[1])
 for i in range(-100,100):
+    mt0.run(i/100)
     mt1.run(i/100)
-    mt2.run(i/100)
     time.sleep(0.02)
