d5b549c3fd20a457b6b0c614c66212a834ccb7cd
school_pi <tantalum_zhao@qq.com>
2022-08-07 05:23:10 -0500

    测试电机

--- motor_test.py
+++ motor_test.py
@@ -6,14 +6,14 @@ ctrl = [[3,5,7],[12,8,10]]
 
 mt0 = motor.Motor(ctrl[0])
 mt1 = motor.Motor(ctrl[1])
-
-for i in range(-100,100):
+'''
+for i in range(0,100):
     mt0.run(i/100)
     mt1.run(i/100)
     time.sleep(0.02)
-
+'''
 mt0.run(1)
-mt1.run(1)
+mt1.run(-1)
 time.sleep(1)
 mt0.run(0)
 mt1.run(0)
