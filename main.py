cd7e9ccc59eaea674fc6e4aa99e57535e1641e59
school_pi <tantalum_zhao@qq.com>
2022-08-09 12:49:02 -0500

    完善通信处理，增加保护机制

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
-import motor,time,serial
+import time
+
 import numpy as np
 import RPi.GPIO as gpio
+import serial
+
+import motor
 
 
-#流程：
-#开机初始化
-#循环接受数据
-#PID调速
-#开串口
-ser = serial.Serial("/dev/ttyAMA0", 115200)
-#设置GPIO编号模式:根据PCB编号
-gpio.setmode(gpio.BOARD)
-#清空40Pin所有使用引脚,以便后续复用
-#巨坑！
-gpio.cleanup([7])
-gpio.cleanup([x for x in range(1,40)])
-
+#串口发送
 def serial_read(count=0):
     while not count:
         # 没有收到字符就重复获取等待
@@ -46,44 +38,85 @@ def init_motor(sonic_en=7):
     #因为开机启动有延迟，所以在执行到这里的第一时间就要把开机的信息告诉传感器
     #本来是打算做一个串口通信的
     #后来一想直接做一个使能端即可（具体逻辑还要抓抓波形看看）
+    #使能端本来打算直接接到传感器MCU复位引脚上
+    #但经过分析发现,为了保障稳定性，还是采用通信比较安全
     gpio.setmode(gpio.BOARD)
-    gpio.setup(sonic_en, gpio.OUT)
-    gpio.output(sonic_en, gpio.HIGH)
+    #gpio.setup(sonic_en, gpio.OUT)
+    #gpio.output(sonic_en, gpio.HIGH)
     while True:
-        recv = serial_read()  
+        #recv = serial_read()  
         #print(recv)
         #print((recv == b"steste")-10086)
-        if recv == b"steste":
+        ser.write("shelloe")
+        if serial_read() == b"shelloe":
             break
 
+    while True:
+        #recv = serial_read()  
+        #print(recv)
+        #print((recv == b"steste")-10086)
+        if serial_read() == b"steste":
+            break
+
+#全流程：
+#开机初始化
+#循环接受数据
+#PID调速
+
+#开串口
+ser = serial.Serial("/dev/ttyAMA0", 115200)
+#设置GPIO编号模式:根据PCB编号
+gpio.setmode(gpio.BOARD)
+
+#清空40Pin所有使用引脚,以便后续复用
+#巨坑！
+#实际使用后发现,该方法还是不行,cleanup函数还是没有权限清空之前程序调用的gpio模式，这意味着……若要清空似乎还是要麻烦一些的
+#但是考虑到实际使用中，不存在GPIO复用的情况这个问题也就不存在了
+#但是也是确实存在之前测试时复用GPIO产生的无法通信问题
+#gpio.cleanup([7])
+#gpio.cleanup([x for x in range(1,40)])
+
+
 if __name__ == '__main__':
+    
     while True:
         init_motor()
         try:
+            #尝试建立平台类
             pf = motor.Platform([[11,13,15],[22,16,18]])
+            pf.test()
         except :
             ser.write(b"s1e")
         else :
             ser.write(b"s0e")
             break
     #print("!!!!!!")
-    data=[[0.0], [0.0]]
+    data=[[0.0],[0.0], [0.0]]
+    error_times = 0
+    stop_flag = False
     while True:
         try:
-            while True:
+            temp=[]
+            for index in range(3):
                 if serial_read() == b"s":
-                    data[0].append(float(serial_read()))
-                    data[1].append(float(serial_read()))
-                    #print(data)
-                    pf.RUN(data)
+                    temp.append(float(serial_read()))
+                    #print(data[index])
                     _ = serial_read()
-            gpio.cleanup([x for x in range(1,40)])
+            
+            pf.RUN(data)
+            error_times = error_times-1 if error_times else error_times
         except KeyboardInterrupt:
             if ser != None:
                 ser.close()
                 gpio.cleanup([x for x in range(1,40)])
-        except ValueError:
+        except ValueError :
             #出现这个报错应该是因为数据传输出问题了
             #可能需要检查接线
             #可以的话可以再加入一个报错灯
+            error_times += 1
+            if not error_times:
+                stop_flag = False
+            if error_times >=100 or stop_flag:
+                pf.stop()
+                stop_flag = True
             pass
